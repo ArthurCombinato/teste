522f756343d9772e53f6e437e45b4b9902ea3a76
ArthurCombinato <arthurcombinatodeabreu@gmail.com>
2026-02-17 21:57:16 -0300

    .

--- SiteAlex/Script.js
+++ SiteAlex/Script.js
@@ -4,27 +4,21 @@ const figura = document.querySelector('.fig1');
 const imagens = document.querySelectorAll('.ft1');
 const btnEsquerda = document.querySelector('.esquerda');
 const btnDireita = document.querySelector('.direita');
-
 let index = 0;
 const maxMovimentos = 4; //  só pode andar 2 vezes
-
 function atualizarCarrosel() {
     const largura = carrosel.clientWidth;
     figura.style.transform = `translateX(-${index * largura}px)`;
 }
-
 btnDireita.addEventListener('click', () => {
     if (index < maxMovimentos) {
         index++;
         atualizarCarrosel();
     }
 });
-
 btnEsquerda.addEventListener('click', () => {
     if (index > 0) {
         index--;
         atualizarCarrosel();
     }
 });
-
-
